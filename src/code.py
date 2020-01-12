4a562d33202ec9ee754de5a0e923a203192f92b2
lseth <58041211+lseth@users.noreply.github.com>
2020-01-12 13:16:09 -0500

    walk or sleep
    finished fish branch. starting 'keep walking or sleep'

--- src/code.py
+++ src/code.py
@@ -23,7 +23,7 @@ def
 def leave_plane
     response = input('As your sight begins to clear, you see a black column raising from the plane. You smell something acrid, do you leave the plane? Yes or no. \n').lower()
     while response not in affirm_resp or neg_resp:
-        response = input('I said yes or no. \n').lower()
+        response = input("This is important. It's a time sensitive crucical decision. Do you walk away from the plane, yes or no?\n").lower()
     if response in affirm_resp:
         return True
     else:
@@ -70,17 +70,36 @@ def hands_spear
     else:
         return True
 
-raw = ['raw', 'salmonell baybey', 'salmonella', 'sam on ella', "don't cook it", "dont cook it"]
+raw = ['raw', 'salmonell baybey', 'salmonella', 'sam on ella', "don't cook it", "dont cook it", 'eat it raw']
 cook = ['cook', 'cook it', 'try cooking it', 'i cook it', 'fire', 'my mixtape']
 def raw_cook
     response = input("Wow, it worked! You feel like Tarzan. You have a slippery fish in your hands. You almost drop it, but don't. Now, do you just eat it raw or try to cook it?\n").lower()
     while response not in :
-        response = input("\n").lower()
-    if response in :
+        response = input("You focused so much on catching the fish. You're so excited, all you think about is eat it raw or cook it?\n").lower()
+    if response in raw:
         return True
     else:
         return False
 
+clothes = ['clothes', 'my clothes', 'the clothes', 'try to use my clothes']
+wood = ['wood', 'wood nearby', 'nearby wood', 'try to find food nearby']
+def fire
+    response = input("You may be in the wilderness, but even these waters have been polluted by humans. Good choice. Now you just need some kindling to start the fire. You have your clothes or you can try to find wood nearby. Which do you choose? Wood or clothes? \n").lower()
+    while response not in clothes or wood :
+        response = input("You got a little dizzy catching the fish. Try to stick to just clothes or wood. \n").lower()
+    if response in clothes:
+        return True
+    else:
+        return False
+
+def berries
+    response = input("Now, do you try to find berries? You think you remember your scout training on how to tell if a berry is poisonous\n").lower()
+    while response not in affirm_resp or neg_resp:
+        response = input("This is a simple decision and needs a simple answer. Do you try to scavenge berries?\n").lower()
+    if response in affirm_resp:
+        return True
+    else:
+        return False
 
 while true 
     print walk message
@@ -97,5 +116,14 @@ wind_ch = up_down_wind
 #leave river is false and survive
 #need to leave river w false to survive
 river_ch = follow_river
-fish_ch = follow_river
+fish_ch = catch_fish
 #hands and spear are both true, it doesn't matter. they both succeed
+hand_spear_ch = hands_spear
+#raw is true
+#cooked is false and goes to fire
+raw_cook_ch = raw_cook
+#clothes is true and death
+#wood is false and also death
+fire_ch = fire
+#getting berries is true and death
+berries_ch = berries
